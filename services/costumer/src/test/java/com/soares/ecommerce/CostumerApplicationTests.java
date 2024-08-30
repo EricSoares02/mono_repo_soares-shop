e54ebd7c459c9714251ff3db002d3e268dae1c93
Eric Soares <ericsoareslopesdev@gmail.com>
2024-08-30 19:23:37 -0300

    feat: add costumer service to eureka log

---
+++ services/costumer/src/test/java/com/soares/ecommerce/CostumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soares.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
